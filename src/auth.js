f462a6c823df0abaee89f0824f1e4197d034df17
kekedezha <dezhac6@gmail.com>
2025-06-19 13:26:33 -0400

    corrected syntax mistakes

--- src/auth.js
+++ src/auth.js
@@ -16,9 +16,9 @@ router.post("/login", async (req, res) => {
     if (result.rows.length === 0) {
       return res
         .status(404)
-        .json({ error: "User/username not found. Please try again." });
+        .json({ error: "Invalid credentials. Please try again." });
     }
-    user = result.rows[0];
+    const user = result.rows[0];
     const validPassword = await bcrypt.compare(password, user.password_hash);
 
     if (!validPassword) {
@@ -31,7 +31,7 @@ router.post("/login", async (req, res) => {
     };
     const secret = process.env.JWT_SECRET;
     const options = { expiresIn: "30m" };
-    token = jwt.sign(payload, secret, options);
+    const token = jwt.sign(payload, secret, options);
     res.status(200).json({ token });
   } catch (err) {
     console.log("Error logging in user.", err);
